68117d7916a6d68ad025381e8461a1522ccf0ab6
[Nicholas Dobos] <Ndobos95@gmail.com>
2024-11-12 01:03:37 -0500

    fix mobile nav

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -72,16 +72,21 @@ const MobileNav = () => {
                 ref={navRef}
                 className="mt-8 flex h-full basis-0 flex-col items-start overflow-y-auto pl-12 pt-2 text-left"
               >
-                {headerNavLinks.map((link) => (
-                  <Link
-                    key={link.title}
-                    href={link.href}
-                    className="mb-4 py-2 pr-4 text-2xl font-bold tracking-widest text-gray-900 outline outline-0 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400"
-                    onClick={onToggleNav}
-                  >
-                    {link.title}
-                  </Link>
-                ))}
+                {headerNavLinks
+                  .filter(
+                    (link) =>
+                      link.href !== '/' && link.title !== 'About' && link.title !== 'Projects'
+                  )
+                  .map((link) => (
+                    <Link
+                      key={link.title}
+                      href={link.href}
+                      className="mb-4 py-2 pr-4 text-2xl font-bold tracking-widest text-gray-900 outline outline-0 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400"
+                      onClick={onToggleNav}
+                    >
+                      {link.title}
+                    </Link>
+                  ))}
               </nav>
 
               <button
