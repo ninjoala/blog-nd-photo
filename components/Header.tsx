7f189fd2cb6b7541d96021bd309259f92f8c4384
[Nicholas Dobos] <Ndobos95@gmail.com>
2024-11-12 00:53:32 -0500

    fixing formatting for linter

--- components/Header.tsx
+++ components/Header.tsx
@@ -31,7 +31,9 @@ const Header = () => {
       <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
         <div className="no-scrollbar hidden max-w-40 items-center space-x-4 overflow-x-auto sm:flex sm:space-x-6 md:max-w-72 lg:max-w-96">
           {headerNavLinks
-            .filter((link) => link.href !== '/' && link.title !== 'About' && link.title !== 'Projects')
+            .filter(
+              (link) => link.href !== '/' && link.title !== 'About' && link.title !== 'Projects'
+            )
             .map((link) => (
               <Link
                 key={link.title}
